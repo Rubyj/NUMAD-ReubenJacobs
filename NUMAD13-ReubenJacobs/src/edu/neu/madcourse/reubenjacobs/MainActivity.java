e80e840c2aa280a30182e2a37a8de08f8b078607
Reuben Jacobs <reuben.jacobs1@gmail.com>
2013-09-15 22:55:13 -0400

    Initial upload of files in workspace. Assignment 1.

---
+++ NUMAD13-ReubenJacobs/src/edu/neu/madcourse/reubenjacobs/MainActivity.java
@@ -0,0 +1,54 @@
+package edu.neu.madcourse.reubenjacobs;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+import edu.neu.mobileClass.*;
+
+public class MainActivity extends Activity {
+	
+	public final static String EXTRA_MESSAGE = "edu.neu.madcourse.reubenjacobs.MESSAGE";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		PhoneCheckAPI.doAuthorization(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	/** Called when the user clicks the Send button */
+	public void sendMessage(View view) {
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		startActivity(intent);
+	}
+	
+	public void generateError(View view) {
+		int x = 5 / 0;
+	}
+	
+	public void exit(View view) {
+		Intent intent = new Intent(Intent.ACTION_MAIN);
+		intent.addCategory(Intent.CATEGORY_HOME);
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		startActivity(intent);
+	}
+	
+	public void startSudoku(View view) {
+		Intent intent = new Intent(this, Sudoku.class);
+		startActivity(intent);
+	}
+
+}
