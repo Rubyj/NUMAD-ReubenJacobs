62b600e7f49caabdd9e3f21a6607de388ab11e93
Reuben Jacobs <reuben.jacobs1@gmail.com>
2013-09-22 20:24:06 -0400

    Changed layout of buttons to be relative to screen size (margins) - THIS WAS EXTREMELY TIME CONSUMING TO FIGURE OUT, very hard...

--- NUMAD13-ReubenJacobs/src/edu/neu/madcourse/reubenjacobs/MainActivity.java
+++ NUMAD13-ReubenJacobs/src/edu/neu/madcourse/reubenjacobs/MainActivity.java
@@ -1,14 +1,16 @@
 package edu.neu.madcourse.reubenjacobs;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.telephony.TelephonyManager;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.Menu;
 import android.view.View;
-import android.widget.TextView;
-import edu.neu.mobileClass.*;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
 
 public class MainActivity extends Activity {
 	
@@ -17,7 +19,39 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		Display display = getWindowManager().getDefaultDisplay();
+		DisplayMetrics outMetrics = new DisplayMetrics();
+		display.getMetrics(outMetrics);
+		
+		/*
+		float density = getResources().getDisplayMetrics().density;
+		float dpHeight = outMetrics.heightPixels / density;
+		float dpWidth = outMetrics.widthPixels / density;
+		*/
 		setContentView(R.layout.activity_main);
+		
+		Button aboutButton = (Button) this.findViewById(R.id.aboutButton);
+		Button sudokuButton = (Button) this.findViewById(R.id.sudokuButton);
+		Button errorButton = (Button) this.findViewById(R.id.errorButton);
+		Button quitButton = (Button) this.findViewById(R.id.quitButton);
+		
+		LayoutParams aboutParams = (RelativeLayout.LayoutParams) aboutButton.getLayoutParams();
+		aboutParams.setMargins((int)outMetrics.widthPixels/3, (int)outMetrics.heightPixels/15, (int)outMetrics.widthPixels/3, (int)outMetrics.heightPixels/15);
+		aboutButton.setLayoutParams(aboutParams);
+		
+		LayoutParams sudokuParams = (RelativeLayout.LayoutParams) sudokuButton.getLayoutParams();
+		sudokuParams.setMargins(0, 0, (int)outMetrics.widthPixels/3, 0);
+		sudokuButton.setLayoutParams(sudokuParams);
+		
+		LayoutParams errorParams = (RelativeLayout.LayoutParams) errorButton.getLayoutParams();
+		errorParams.setMargins(0, (int)outMetrics.heightPixels/15, (int)outMetrics.widthPixels/3, 0);
+		errorButton.setLayoutParams(errorParams);
+		
+		LayoutParams quitParams = (RelativeLayout.LayoutParams) quitButton.getLayoutParams();
+		quitParams.setMargins(0, (int)outMetrics.heightPixels/15, (int)outMetrics.widthPixels/3, 0);
+		quitButton.setLayoutParams(quitParams);
+		
 		
 		//Uncomment for authorization
 		//PhoneCheckAPI.doAuthorization(this); 
