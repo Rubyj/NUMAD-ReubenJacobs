017bf49a7c49c16087113b25a34e38d0b41153a6
Dan Kreymer <dkreymer@gmail.com>
2013-09-23 18:41:30 -0400

    initial version of dictionary layout

--- NUMAD13-DanKreymer/src/edu/neu/madcourse/dankreymer/Dictionary.java
+++ NUMAD13-DanKreymer/src/edu/neu/madcourse/dankreymer/Dictionary.java
@@ -2,13 +2,27 @@ package edu.neu.madcourse.dankreymer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class Dictionary extends Activity {
+public class Dictionary extends Activity implements OnClickListener{
 	private static final String TAG = "Dictionary";
 	
 	   public void onCreate(Bundle savedInstanceState) 
 	   {
 		      super.onCreate(savedInstanceState);
 		      setContentView(R.layout.dictionary);
+		      
+		      View wordBox = findViewById(R.id.word_box);
+		      wordBox.setOnClickListener(this);
 	   }
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case (R.id.word_box):
+			
+		}
+		
+	}
 }
