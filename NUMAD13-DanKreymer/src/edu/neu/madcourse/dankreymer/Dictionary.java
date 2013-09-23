bc132940e9378adee387495f3afbde994c18b245
Dan Kreymer <dkreymer@gmail.com>
2013-09-23 03:49:49 -0400

    New Dictionary Files

---
+++ NUMAD13-DanKreymer/src/edu/neu/madcourse/dankreymer/Dictionary.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.dankreymer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Dictionary extends Activity {
+	private static final String TAG = "Dictionary";
+	
+	   public void onCreate(Bundle savedInstanceState) 
+	   {
+		      super.onCreate(savedInstanceState);
+		      setContentView(R.layout.dictionary);
+	   }
+}
