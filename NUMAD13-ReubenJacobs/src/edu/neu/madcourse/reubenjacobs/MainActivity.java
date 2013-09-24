8e2a52339d7747bd70e7d8553d2ad1aaf1c4d8f1
Reuben Jacobs <reuben.jacobs1@gmail.com>
2013-09-24 00:49:09 -0400

    new version codes

--- NUMAD13-ReubenJacobs/src/edu/neu/madcourse/reubenjacobs/MainActivity.java
+++ NUMAD13-ReubenJacobs/src/edu/neu/madcourse/reubenjacobs/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends Activity {
 		
 		
 		//Uncomment for authorization
-		//PhoneCheckAPI.doAuthorization(this); 
+		PhoneCheckAPI.doAuthorization(this); 
 	}
 
 	@Override
