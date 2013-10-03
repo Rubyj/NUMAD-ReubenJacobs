a3692fac9b46cabb7ad443ffa7575eb77f37eeee
Dan Kreymer <dkreymer@gmail.com>
2013-10-02 21:24:51 -0400

    setting up dabble data structures

--- NUMAD13-DanKreymer/src/edu/neu/madcourse/dankreymer/DabbleGame.java
+++ NUMAD13-DanKreymer/src/edu/neu/madcourse/dankreymer/DabbleGame.java
@@ -1,5 +1,62 @@
 package edu.neu.madcourse.dankreymer;
 
-public class DabbleGame {
+import java.util.ArrayList;
+import java.util.Random;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class DabbleGame extends Activity {
+	private static final String TAG = "Dabble";
+	
+	private static int numTiles = 18;
+
+	private DabbleView dabbleView;
+	
+	private ArrayList<String> solution;
+	private char[] tiles;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		generateSolution();
+		generateTiles();
+		
+
+		dabbleView = new DabbleView(this);
+		setContentView(dabbleView);
+		dabbleView.requestFocus();
+
+	}
+	
+	private void generateSolution(){
+		//TODO: use dictionary!
+		solution.add("cow");
+		solution.add("plop");
+		solution.add("crops");
+		solution.add("lemons");
+	}
+	
+	private void generateTiles(){
+		String wordsCombined = solution.get(0) + solution.get(1) + solution.get(2) + solution.get(3);
+		Random rand = new Random();
+		
+		tiles = new char[numTiles];
+		for (int i = 0; i < numTiles; i++)
+		{
+			tiles[i] = wordsCombined.charAt(rand.nextInt(18));
+		}
+	}
+	
+	private String tilesToString()
+	{
+		return new String(tiles);
+	}
+	
+	private void StringToTiles(String string)
+	{
+		tiles = string.toCharArray();
+	}
 }
