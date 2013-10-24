95809e9c97d3a086bfeb2c181d6f8ebacff32bff
Dan Kreymer <dkreymer@gmail.com>
2013-10-24 02:53:47 -0400

    Adding new files

---
+++ NUMAD13-DanKreymer/src/edu/neu/madcourse/dankreymer/multiplayer/DabbleMTurnBased.java
@@ -0,0 +1,17 @@
+package edu.neu.madcourse.dankreymer.multiplayer;
+
+import edu.neu.madcourse.dankreymer.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DabbleMTurnBased extends Fragment{
+
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+        Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dabble_m_turnbased, container, false);
+    }
+}
