86e4a65bfc39a90234cb0e9c94ba98fc80f30648
Dan Kreymer <dkreymer@gmail.com>
2013-11-12 22:37:13 -0500

    enable phone survey button toggling

--- NUMAD13-DanKreymer/src/edu/neu/madcourse/dankreymer/trickystress/TrickyStressMain.java
+++ NUMAD13-DanKreymer/src/edu/neu/madcourse/dankreymer/trickystress/TrickyStressMain.java
@@ -7,18 +7,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class TrickyStressMain extends Activity implements OnClickListener {
-
+	private boolean enabled;
+	private Button phoneSurveyButton;
+	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.tricky_stress_main);
+		
+		enabled = false;
 		
 		View quitButton = findViewById(R.id.tricky_quit_button);
 		quitButton.setOnClickListener(this);
 		View periodicSurveyButton = findViewById(R.id.tricky_periodic_survey_button);
 		periodicSurveyButton.setOnClickListener(this);
-		View phoneSurveyButton = findViewById(R.id.tricky_phone_survey_button_label);
+		
+		phoneSurveyButton = (Button) findViewById(R.id.tricky_phone_survey_button);
 		phoneSurveyButton.setOnClickListener(this);
 	}
 	
@@ -29,9 +35,26 @@ public class TrickyStressMain extends Activity implements OnClickListener {
 			Intent intent = new Intent(this, TrickyStressBT.class);
 			startActivity(intent);
 			break;
+		case R.id.tricky_phone_survey_button:
+			togglePhoneSurveyButton();
+			break;
 		case R.id.tricky_quit_button:
 			finish();
 			break;
+		}
+	}
+	
+	private void togglePhoneSurveyButton()
+	{
+		enabled = !enabled;
+		
+		if (enabled)
+		{
+			phoneSurveyButton.setText(getResources().getString(R.string.tricky_phone_survey_button_disable_label));
+		}
+		else
+		{
+			phoneSurveyButton.setText(getResources().getString(R.string.tricky_phone_survey_button_label));
 		}
 	}
 }
