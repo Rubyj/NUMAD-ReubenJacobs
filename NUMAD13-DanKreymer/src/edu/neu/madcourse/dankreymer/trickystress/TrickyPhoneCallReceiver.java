6ea43c2e15daf3b911374a7813b7aae1a54a79fb
Dan Kreymer <dkreymer@gmail.com>
2013-11-13 01:12:18 -0500

    outgoing calls prompt the survey before the call as well

--- NUMAD13-DanKreymer/src/edu/neu/madcourse/dankreymer/trickystress/TrickyPhoneCallReceiver.java
+++ NUMAD13-DanKreymer/src/edu/neu/madcourse/dankreymer/trickystress/TrickyPhoneCallReceiver.java
@@ -12,7 +12,7 @@ public class TrickyPhoneCallReceiver extends BroadcastReceiver {
 	public void onReceive(Context context, Intent intent) {
 	    String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
 	    
-	    if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+	    if (state.equals(TelephonyManager.EXTRA_STATE_RINGING) || state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
 	    	String number = intent.getExtras().getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
 	    	
 	    	SharedPreferences.Editor editor = context.getSharedPreferences("prefs", Context.MODE_PRIVATE).edit();
